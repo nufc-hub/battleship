7d69ee19842d460cdcdf0efe6187c30dba548a88
nufc-hub <kmchenouf@gmail.com>
2024-11-12 22:17:49 +0900

    Add constructor with length property

--- src/ship.js
+++ src/ship.js
@@ -1,3 +1,17 @@
-class Ship {}
+class Ship {
+  /**
+   *
+   */
+  constructor(length) {
+    this.length = length;
+    // this.numberOfHits = 0;
+    // this.sunk = false;
+  }
+
+  // hit() function that increases the number of ‘hits’ in your ship.
+
+  // isSunk() should be a function that calculates whether a ship is
+  // considered sunk based on its length and the number of hits it has received.
+}
 
 export default Ship;
