3f3ade7c6d3a6b83a733bbf290e8df8e97f8628f
nufc-hub <kmchenouf@gmail.com>
2024-11-12 22:42:41 +0900

    Add test for ship.numberOfHits

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,5 +1,7 @@
 import Ship from '../src/ship';
 
+// Test ship length.
+
 test('return ship length with a value of 3', () => {
   const ship = new Ship(3);
 
@@ -10,4 +12,12 @@ test('return ship length with a value of 3', () => {
   const ship = new Ship(0);
 
   expect(ship.length).toBe(0);
+});
+
+// Test times ship has been hit.
+
+test('number of hits will be 0', () => {
+  const ship = new Ship(3);
+
+  expect(ship.numberOfHits).toBe(0);
 });
