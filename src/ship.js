c170f475f861e111304ca04145631eb0f85b38f5
nufc-hub <kmchenouf@gmail.com>
2024-11-13 16:45:07 +0900

    Add isSunk() function

--- src/ship.js
+++ src/ship.js
@@ -4,15 +4,15 @@ class Ship {
     this.numberOfHits = 0;
   }
 
-  // Throw error if a length of <= 0 or >= 4 is inputted.
-
   // Increases number of hits on ship.
   hit() {
     this.numberOfHits += 1;
   }
 
-  // Returns true if the ship has been sunk.
-  isSunk() {}
+  // Returns true if the ship has been sunk, otherwise false.
+  isSunk() {
+    return this.numberOfHits >= this.length;
+  }
 }
 
 export default Ship;
