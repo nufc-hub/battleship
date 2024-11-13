dbfe0ed66e2009490e4b4bbb7fb4ae7fcea83d32
nufc-hub <kmchenouf@gmail.com>
2024-11-13 16:43:20 +0900

    Add tests for ship.isSunk()

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -38,3 +38,28 @@ test('hit increases the number of hits to 2', () => {
   ship.hit();
   expect(ship.numberOfHits).toBe(2);
 });
+
+// Tests if the ship has been sunk.
+
+test('calculates if a ship of length 3 has been sunk after 0 hits', () => {
+  const ship = new Ship(3);
+  expect(ship.isSunk()).toBe(false); // After 0 hits
+});
+
+test('calculates if a ship of length 3 has been sunk after 3 hits', () => {
+  const ship = new Ship(3);
+  ship.hit(); //
+  expect(ship.isSunk()).toBe(false); // After 1 hit
+  ship.hit();
+  expect(ship.isSunk()).toBe(false); // After 2 hits
+  ship.hit();
+  expect(ship.isSunk()).toBe(true); // After 3 hits
+});
+
+test('calculates if a ship of length 2 has been sunk after 2 hits', () => {
+  const ship = new Ship(2);
+  ship.hit();
+  expect(ship.isSunk()).toBe(false); // After 1 hit
+  ship.hit();
+  expect(ship.isSunk()).toBe(true); // After 2 hits
+});
