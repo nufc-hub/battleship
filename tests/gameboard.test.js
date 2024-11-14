84bcdd32450bd8d4bb1264562f14b9997b598a03
nufc-hub <kmchenouf@gmail.com>
2024-11-14 22:54:47 +0900

    Test horizontal ship placement

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,4 +1,5 @@
 import GameBoard from '../src/gameboard';
+import Ship from '../src/ship';
 
 // Test the game board's length
 
@@ -11,4 +12,22 @@ test('initialises a 10 x 10 game board with all cells set to 0', () => {
     expect(row.length).toBe(10); // Each row contains exactly 10 items
     expect(row.every((cell) => cell === 0)).toBe(true); //  Value in each cell is 0
   });
+});
+
+// Verify horizontal ship placement
+test('places a horizontal ship on a game board', () => {
+  const horizontalShip = new Ship(3, true);
+  const gameBoard = new GameBoard();
+  gameBoard.placeShip(2, 3, horizontalShip);
+
+  // Verify the correct cells have been changed to 1
+  expect(gameBoard.board[1][2]).toBe(1); // If ship placed, value will be 1 not 0
+  expect(gameBoard.board[1][3]).toBe(1);
+  expect(gameBoard.board[1][4]).toBe(1);
+
+  // Verify other cells are still 0
+  expect(gameBoard.board[2][2]).toBe(0); // Checking cells close to ship for unintended consequences
+  expect(gameBoard.board[2][6]).toBe(0);
+  expect(gameBoard.board[1][5]).toBe(0);
+  expect(gameBoard.board[1][1]).toBe(0);
 });
