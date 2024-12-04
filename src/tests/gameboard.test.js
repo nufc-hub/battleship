e362653a6b56fcf9cff43597db8ea79f91ce7795
KC <121441283+nufc-hub@users.noreply.github.com>
2024-12-04 21:22:48 +0900

    Move files into src/tests

---
+++ src/tests/gameboard.test.js
@@ -0,0 +1,191 @@
+import GameBoard from '../src/gameboard';
+import Ship from '../src/ship';
+
+// Test the game board's length
+
+test('initialises a 10 x 10 game board with all cells set to 0', () => {
+  const gameBoard = new GameBoard();
+
+  expect(gameBoard.board.length).toBe(10); // Exactly 10 rows in game board
+
+  gameBoard.board.forEach((row) => {
+    expect(row.length).toBe(10); // Each row contains exactly 10 items
+    expect(row.every((cell) => cell === 0)).toBe(true); //  Value in each cell is 0
+  });
+});
+
+// Verify horizontal ship placement
+test('places a horizontal ship on a game board', () => {
+  const horizontalShip = new Ship(3, true);
+  const gameBoard = new GameBoard();
+  gameBoard.placeShip(2, 3, horizontalShip);
+
+  // Verify the correct cells have been changed to 1
+  expect(gameBoard.board[1][2]).toBe(1); // If ship placed, value will be 1 not 0
+  expect(gameBoard.board[1][3]).toBe(1);
+  expect(gameBoard.board[1][4]).toBe(1);
+
+  // Verify other cells are still 0
+  expect(gameBoard.board[2][2]).toBe(0); // Checking cells close to ship for unintended consequences
+  expect(gameBoard.board[2][6]).toBe(0);
+  expect(gameBoard.board[1][5]).toBe(0);
+  expect(gameBoard.board[1][1]).toBe(0);
+});
+
+// Verify vertical ship placement
+test('places a vertical ship on a game board', () => {
+  const verticalShip = new Ship(3, false);
+  const gameBoard = new GameBoard();
+  gameBoard.placeShip(2, 3, verticalShip);
+
+  // Verify the correct cells have been changed to 1
+  expect(gameBoard.board[1][2]).toBe(1); // If ship placed, value will be 1 not 0
+  expect(gameBoard.board[2][2]).toBe(1);
+  expect(gameBoard.board[3][2]).toBe(1);
+
+  // Verify other cells are still 0
+  expect(gameBoard.board[0][2]).toBe(0); // Checking cells close to ship for unintended consequences
+  expect(gameBoard.board[4][2]).toBe(0);
+  expect(gameBoard.board[1][3]).toBe(0);
+  expect(gameBoard.board[3][3]).toBe(0);
+});
+
+// Verify horizontal ship is within board boundaries
+
+test('throws an error for out-of-bounds horizontal placement', () => {
+  const gameBoard = new GameBoard();
+  const horizontalShip = new Ship(3, true); // Ship of length 3, horizontal
+
+  expect(() => {
+    gameBoard.placeShip(2, 9, horizontalShip); // Attempt to place the ship
+  }).toThrow('Ship placement is out of bounds.');
+});
+
+test('throws an error for overlapping ship placement', () => {
+  const gameBoard = new GameBoard();
+  const firstShip = new Ship(3, true); // Ship of length 3, horizontal
+  const overlappingShipHorizontal = new Ship(2, true); // Ship of length 2, horizontal
+  const overlappingShipVertical = new Ship(3, false); // Ship of length 3, vertical
+
+  gameBoard.placeShip(2, 3, firstShip); // Place the first ship at row 2 column 3
+
+  // Horizontal
+  expect(() => {
+    gameBoard.placeShip(2, 4, overlappingShipHorizontal); // Attempt to place another ship at row 2 column 3
+  }).toThrow('Ship placement overlaps with another ship.');
+
+  // Vertical
+  expect(() => {
+    gameBoard.placeShip(1, 3, overlappingShipVertical); // Attempt to place another ship at row 2 column 3
+  }).toThrow('Ship placement overlaps with another ship.');
+});
+
+// Test attacks
+
+test('registers a hit on a ship', () => {
+  const horizontalShip = new Ship(3, true);
+  const gameBoard = new GameBoard();
+  gameBoard.placeShip(2, 3, horizontalShip);
+
+  // Attack and hit ship
+  gameBoard.receiveAttack(2, 3, horizontalShip);
+
+  expect(gameBoard.board[1][2]).toBe(-1);
+
+  // Check ship's number of hits property
+  expect(horizontalShip.numberOfHits).toBe(1);
+});
+
+test('registers a miss on the board', () => {
+  const horizontalShip = new Ship(3, true);
+  const gameBoard = new GameBoard();
+  gameBoard.placeShip(2, 3, horizontalShip);
+
+  // Attack and miss
+  gameBoard.receiveAttack(1, 2);
+
+  expect(gameBoard.board[1][2]).toBe(1); // Attack hit empty coord so value will still be 1
+  expect(gameBoard.board[0][1]).toBe(-2); // Miss is marked as -2 as no ship was there
+});
+
+test('prevents double counting a hit', () => {
+  const horizontalShip = new Ship(3, true);
+  const gameBoard = new GameBoard();
+  gameBoard.placeShip(2, 3, horizontalShip);
+
+  // Double hit
+  gameBoard.receiveAttack(2, 3);
+
+  expect(gameBoard.receiveAttack(2, 3)).toEqual(
+    {
+      message: 'This position has already been attacked!',
+    } // Attack hit ship again
+  );
+  expect(horizontalShip.numberOfHits).toBe(1); // Number of hits should remain the same
+  expect(gameBoard.board[1][2]).toBe(-1); // Attack hit empty coord so value will still be 1
+});
+
+test('prevents double counting a miss', () => {
+  const horizontalShip = new Ship(3, true);
+  const gameBoard = new GameBoard();
+  gameBoard.placeShip(2, 3, horizontalShip);
+
+  // Double miss
+  gameBoard.receiveAttack(1, 2); // Attack hit empty coord
+
+  expect(gameBoard.receiveAttack(1, 2)).toEqual(
+    {
+      message: 'This position has already been attacked!',
+    } // Attack hit empty coord again
+  );
+  expect(gameBoard.board[0][1]).toBe(-2);
+});
+
+// Tests if ship has been sunk
+
+test('return a ship sunk message if a ship is sunk and decrement remainingShips', () => {
+  const horizontalShip = new Ship(3, true); // Ship of length three
+  const verticalShip = new Ship(3, false);
+  const gameBoard = new GameBoard();
+  gameBoard.placeShip(2, 3, horizontalShip);
+  gameBoard.placeShip(4, 5, verticalShip);
+
+  gameBoard.receiveAttack(2, 3, horizontalShip); // First hit
+  gameBoard.receiveAttack(2, 4, horizontalShip); // Second hit
+  // Third and final hit
+  expect(gameBoard.receiveAttack(2, 5, horizontalShip)).toEqual({
+    message: 'A ship has been sunk',
+    remainingShips: gameBoard.remainingShips,
+  });
+
+  expect(gameBoard.remainingShips).toBe(1);
+});
+
+// Test if a game is over
+test('return a game over message if all ships sunk and decrement remainingShips', () => {
+  const horizontalShip = new Ship(3, true); // Ship of length three
+  const verticalShip = new Ship(3, false);
+  const gameBoard = new GameBoard();
+
+  // Remaining ships will be 2
+  gameBoard.placeShip(2, 3, horizontalShip);
+  gameBoard.placeShip(4, 5, verticalShip);
+
+  // Sink first ship
+  // Remaining ships will be 1
+  gameBoard.receiveAttack(2, 3); // First hit
+  gameBoard.receiveAttack(2, 4); // Second hit
+  gameBoard.receiveAttack(2, 5); // Third and final hit
+
+  // Sink second ship
+  gameBoard.receiveAttack(4, 5); // First hit
+  gameBoard.receiveAttack(5, 5); // Second hit
+
+  // Third and final hit
+  expect(gameBoard.receiveAttack(6, 5)).toEqual({
+    message: 'Game over! All ships have been sunk!',
+    remainingShips: 0,
+  });
+
+  expect(gameBoard.remainingShips).toBe(0);
+});
---
+++ src/tests/ship.test.js
@@ -0,0 +1,79 @@
+import Ship from '../src/ship';
+
+// Test ship length.
+
+test('return ship length with a value of 3', () => {
+  const ship = new Ship(3, true);
+
+  expect(ship.length).toBe(3);
+});
+
+test('return ship length with a value of 0', () => {
+  const ship = new Ship(0, true);
+
+  expect(ship.length).toBe(0);
+});
+
+// Test ship orientation.
+
+test('Orientation will be horizontal', () => {
+  const ship = new Ship(3, true); // isHorizontal is set to true as a parameter
+
+  expect(ship.isHorizontal).toBe(true);
+});
+
+test('Orientation will not be horizontal', () => {
+  const ship = new Ship(3, false); // isHorizontal is set to false as a parameter
+
+  expect(ship.isHorizontal).toBe(false);
+});
+
+// Test times ship has been hit.
+
+test('number of hits will be 0', () => {
+  const ship = new Ship(3, true);
+
+  expect(ship.numberOfHits).toBe(0);
+});
+
+// Tests if the ship has been hit.
+
+test('hit increases the number of hits to 1', () => {
+  const ship = new Ship(3, true);
+  //One hit.
+  ship.hit();
+  expect(ship.numberOfHits).toBe(1);
+});
+
+test('hit increases the number of hits to 2', () => {
+  const ship = new Ship(3, true);
+  // Two hits.
+  ship.hit();
+  ship.hit();
+  expect(ship.numberOfHits).toBe(2);
+});
+
+// Tests if the ship has been sunk.
+
+test('calculates if a ship of length 3 has been sunk after 0 hits', () => {
+  const ship = new Ship(3, true);
+  expect(ship.isSunk()).toBe(false); // After 0 hits
+});
+
+test('calculates if a ship of length 3 has been sunk after 3 hits', () => {
+  const ship = new Ship(3, true);
+  ship.hit(); //
+  expect(ship.isSunk()).toBe(false); // After 1 hit
+  ship.hit();
+  expect(ship.isSunk()).toBe(false); // After 2 hits
+  ship.hit();
+  expect(ship.isSunk()).toBe(true); // After 3 hits
+});
+
+test('calculates if a ship of length 2 has been sunk after 2 hits', () => {
+  const ship = new Ship(2, true);
+  ship.hit();
+  expect(ship.isSunk()).toBe(false); // After 1 hit
+  ship.hit();
+  expect(ship.isSunk()).toBe(true); // After 2 hits
+});
