c46c0a2697068df98adc137ba0648c23a4d5304f
KC <121441283+nufc-hub@users.noreply.github.com>
2024-12-04 16:12:25 +0900

    Add tests for initialising a new player

---
+++ tests/player.test.js
@@ -0,0 +1,18 @@
+import Player from '../src/player';
+import GameBoard from '../src/gameboard';
+
+// Real player initialisation
+
+test('initialises a real game player containing a gameBoard', () => {
+  const gameboard = new GameBoard();
+  const humanPlayer = new Player(true, gameboard);
+  expect(humanPlayer.isHuman).toBe(true);
+});
+
+// Computer player initialisation
+
+test('initialises a computer player containing a gameBoard', () => {
+  const gameboard = new GameBoard();
+  const computerPayer = new Player(false, gameboard);
+  expect(computerPayer.isHuman).toBe(false);
+});
