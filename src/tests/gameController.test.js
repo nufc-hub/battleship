05112a7515c28692e2e7215bb49c1c3d7cd5b8a8
KC <121441283+nufc-hub@users.noreply.github.com>
2024-12-05 20:39:23 +0900

    Test for computer player setup

--- src/tests/gameController.test.js
+++ src/tests/gameController.test.js
@@ -20,3 +20,15 @@ test('createHumanPlayer initializes a human player with a gameboard', () => {
     gameBoard: mockGameboard,
   });
 });
+
+// Test computer player setup
+test('createHumanPlayer initializes a human player with a gameboard', () => {
+  const mockGameboard = { board: [], remainingShips: 0, ships: [] }; // Mocked version of gameboard
+  const gameController = new GameController();
+  gameController.createHumanPlayer(mockGameboard);
+
+  expect(gameController.humanPlayer).toEqual({
+    isHuman: true,
+    gameBoard: mockGameboard,
+  });
+});
