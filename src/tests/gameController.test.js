4422dba8fe25120c62e851b7547c6b84d2aac9d6
KC <121441283+nufc-hub@users.noreply.github.com>
2024-12-05 20:35:43 +0900

    Test for human player setup

--- src/tests/gameController.test.js
+++ src/tests/gameController.test.js
@@ -8,3 +8,15 @@ test('createGameboard creates a new gameboard instance', () => {
 
   expect(gameboard).toBeInstanceOf(Gameboard);
 });
+
+// Test human player setup
+test('createHumanPlayer initializes a human player with a gameboard', () => {
+  const mockGameboard = { board: [], remainingShips: 0, ships: [] }; // Mocked version of gameboard
+  const gameController = new GameController();
+  gameController.createHumanPlayer(mockGameboard);
+
+  expect(gameController.humanPlayer).toEqual({
+    isHuman: true,
+    gameBoard: mockGameboard,
+  });
+});
