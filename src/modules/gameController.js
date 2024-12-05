b2a37e9b7ddb2d37297ef661d3009a19c0fd9578
KC <121441283+nufc-hub@users.noreply.github.com>
2024-12-05 20:33:32 +0900

    Create class with constructor

---
+++ src/modules/gameController.js
@@ -0,0 +1,18 @@
+// import Ship from '../modules/ship';
+import Gameboard from '../modules/gameboard';
+import Player from '../modules/player';
+
+// To place a ship:
+// const humanPlayerGameboard = this.humanplayer.gameboard
+// humanPlayerGameboard.placeShip(.., ..)
+
+// Same for computuer placing a ships
+
+class GameController {
+  constructor() {
+    this.humanPlayer = null;
+    this.computerPlayer = null;
+  }
+}
+
+export default GameController;
