89a965250c975419cc3914c31a7f9949d5b07df1
KC <121441283+nufc-hub@users.noreply.github.com>
2024-12-09 23:20:39 +0900

    Add functions to render the initialised game boards

--- src/modules/domManager.js
+++ src/modules/domManager.js
@@ -3,6 +3,29 @@ class DomManager {
     this.gameController = gameController;
   }
 
+  renderHumanGameboard() {
+    this.renderGameboard(this.getHumanGameboard(), 'human-board');
+  }
+
+  renderComputerGameboard() {
+    this.renderGameboard(this.getComputerGameboard(), 'computer-board');
+  }
+
+  renderGameboard(gameboard, boardDivId) {
+    // Loop through gameboard rendering it to webpage
+    gameboard.forEach((row) => {
+      row.forEach((col) => {
+        const humanBoardDiv = document.getElementById(boardDivId);
+        const cell = document.createElement('div');
+        cell.classList = 'board-cell';
+        cell.dataset.value = col; // Placeholder.Change this later to represent the ship there.
+
+        // Append each cell to the board
+        humanBoardDiv.appendChild(cell);
+      });
+    });
+  }
+
   // Get gameboards from players initialised in gameController
 
   getHumanGameboard() {
