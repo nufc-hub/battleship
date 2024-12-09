7febb8de00928e57728bd863a29acb5848dcb634
KC <121441283+nufc-hub@users.noreply.github.com>
2024-12-09 23:19:24 +0900

    Add get gameboard functions

--- src/modules/domManager.js
+++ src/modules/domManager.js
@@ -2,6 +2,18 @@ class DomManager {
   constructor(gameController) {
     this.gameController = gameController;
   }
+
+  // Get gameboards from players initialised in gameController
+
+  getHumanGameboard() {
+    const gameBoard = this.gameController.humanPlayer.gameBoard;
+    return gameBoard.board;
+  }
+
+  getComputerGameboard() {
+    const gameBoard = this.gameController.computerPlayer.gameBoard;
+    return gameBoard.board;
+  }
 }
 
 export default DomManager;
