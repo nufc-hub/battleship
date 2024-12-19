64825cc564c67b9070a0fc6b65fd596d85ae307f
KC <121441283+nufc-hub@users.noreply.github.com>
2024-12-19 15:59:51 +0900

    Add makeComputerAttack method

--- src/modules/player.js
+++ src/modules/player.js
@@ -4,6 +4,36 @@ class Player {
     this.gameBoard = gameBoard;
     this.previousAttack = new Set(); // Track already hit coordinates to avoid duplicate hits
   }
+
+  makeComputerAttack(callback) {
+    // Validations
+    if (this.isHuman) {
+      throw new Error(
+        'makeComputerAttack can only be used by computer players.'
+      );
+    }
+
+    let row, col;
+
+    // Generate a random number between 0 and 9
+    do {
+      row = Math.floor(Math.random() * 10);
+
+      col = Math.floor(Math.random() * 10);
+    } while (this.previousAttack.has(`${row},${col}`)); // Check if the cell has been hit already
+
+    // Prevents attack using the same coords again
+    this.previousAttack.add(`${row},${col}`);
+
+    // Then carry out the attack
+
+    let attackResult = null;
+
+    if (callback && typeof callback === 'function') {
+      attackResult = callback(row, col);
+    }
+    return { row, col, Result: attackResult }; // Return for debugging if needed
+  }
 }
 
 export default Player;
